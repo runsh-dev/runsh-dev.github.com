59ea0f20cecd05b37bea2a1fbe9973f87576bc51
okwfeng <okwfeng@gmail.com>
2024-10-07 14:47:14 +0800

    add google script

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -42,6 +42,19 @@ export default defineConfig({
             "runsh sh script devops",
       },
     ],
+    [
+      'script',
+      {async: '', src: 'https://www.googletagmanager.com/gtag/js?id=G-7RNEYK877J'}
+    ],
+    [
+      'script',
+      {},
+      `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-7RNEYK877J');`
+    ]
   ],
   transformHead: (context) => {
     return transformHeadMeta(context)
